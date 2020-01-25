05c2005992dd412f3ba93c2b5f1a27790fe59d15
Lukasz Dziedzic <ldziedzic1996@gmail.com>
2020-01-25 21:04:39 +0100

    [ZIP] Fixed bits endians in Compression Method enum

--- src/main/java/com/dziedzic/filecompresser/zip/Entity/CompressionMethod.java
+++ src/main/java/com/dziedzic/filecompresser/zip/Entity/CompressionMethod.java
@@ -9,20 +9,21 @@ import java.util.Optional;
 
 public enum CompressionMethod {
     NO_COMPRESSION(new byte[]{0x0, 0x0}),
-    SHRUNK(new byte[]{0x0, 0x1}),
-    REDUCED_WITH_COMPRESION_FACTOR_1(new byte[]{0x0, 0x2}),
-    REDUCED_WITH_COMPRESION_FACTOR_2(new byte[]{0x0, 0x3}),
-    REDUCED_WITH_COMPRESION_FACTOR_3(new byte[]{0x0, 0x4}),
-    REDUCED_WITH_COMPRESION_FACTOR_4(new byte[]{0x0, 0x5}),
-    IMPLODED(new byte[]{0x0, 0x6}),
-    DEFLATED(new byte[]{0x0, 0x8}),
-    ENHANCED_DEFLATED(new byte[]{0x0, 0x9}),
-    PKWARE_DCL_IMPLODED(new byte[]{0x1, 0x0}),
-    BZIP2(new byte[]{0x1, 0x2}),
-    LZMA(new byte[]{0x1, 0x4}),
-    IBM_TERSE(new byte[]{0x1, 0x8}),
-    IBM_LZ77_Z(new byte[]{0x1, 0x9}),
-    PPMD(new byte[]{0x9, 0x8});
+    SHRUNK(new byte[]{0x1, 0x0}),
+    REDUCED_WITH_COMPRESION_FACTOR_1(new byte[]{0x2, 0x0}),
+    REDUCED_WITH_COMPRESION_FACTOR_2(new byte[]{0x3, 0x0}),
+    REDUCED_WITH_COMPRESION_FACTOR_3(new byte[]{0x4, 0x0}),
+    REDUCED_WITH_COMPRESION_FACTOR_4(new byte[]{0x5, 0x0}),
+    IMPLODED(new byte[]{0x6, 0x0}),
+    DEFLATED(new byte[]{0x8, 0x0}),
+    ENHANCED_DEFLATED(new byte[]{0x9, 0x0}),
+
+    PKWARE_DCL_IMPLODED(new byte[]{0x0, 0x1}),
+    BZIP2(new byte[]{0x2, 0x1}),
+    LZMA(new byte[]{0x4, 0x1}),
+    IBM_TERSE(new byte[]{0x8, 0x1}),
+    IBM_LZ77_Z(new byte[]{0x9, 0x1}),
+    PPMD(new byte[]{0x8, 0x1});
 
     private final byte[] compressionMethodCode;
 
